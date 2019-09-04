276de3a87a1c20de57397e20384cb34142d3ea65
DenisUAL <denisandreev64@gmail.com>
2019-09-04 03:30:32 +0300

    fix try

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import superagent from "superagent";
 import { API_URL } from "../config";
-
+const SERVER_ADDRESS = process.env.API_URL || API_URL;
 export default class ApiClient {
   get(url, params = {}) {
     return this._request({ url, method: "get", params });
@@ -23,7 +23,7 @@ export default class ApiClient {
   }
 
   _request({ url, method, params, body }) {
-    const req = superagent[method](`${API_URL}/${url}`);
+    const req = superagent[method](`${SERVER_ADDRESS}/${url}`);
 
     if (params) req.query(params);
     if (body) req.send(body);
