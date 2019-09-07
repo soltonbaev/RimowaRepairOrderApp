002ffceb746ac90eff7108aa93c3da575fd24022
soltonbaev <iscodingacc@gmail.com>
2019-09-07 16:40:38 -0400

    add checkboxes and dropdown

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -5,6 +5,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import DatePicker from "../DatePicker";
 import Input from "../CustomInput";
+import Select from '@material-ui/core/Select';
 
 function inputField(props) {
   const { input, ...rest } = props;
@@ -18,18 +19,36 @@ const createItemForm = itemNum => {
       <form onSubmit={handleSubmit}>
         <div className="itemForm">
           <div className="row">
-            <Field
-              name="needsBy"
-              component={({ input }) => {
-                return (
-                  <DatePicker
-                    {...input}
-                    value={input.value ? input.value : null}
-                    label="Needs by"
-                  />
-                );
-              }}
-            />
+            <div className="inputField">
+              <Field
+                name="needsBy"
+                component={({ input }) => {
+                  return (
+                    <DatePicker
+                      {...input}
+                      value={input.value ? input.value : null}
+                      label="Needs by"
+                    />
+                  );
+                }}
+              />
+            </div>
+            <div className="inputField">
+              <Field
+                name="walkinOrShipped"
+                defaultValue={true}
+                component={({ input }) => (
+                  <Select
+                    native
+                  >
+                    <option value="Select an option">Origin (select an option)</option>
+                    <option value="walkIn">Walk in</option>
+                    <option value="shippedToStore">Shipped to store</option>
+                  </Select>
+
+                )}
+              />
+            </div>
           </div>
           <div className="row">
             <Field
@@ -63,24 +82,70 @@ const createItemForm = itemNum => {
               component={inputField}
             />
           </div>
-          <div className="checkboxContainer">
-            <Field
-              name="warranty"
-              defaultValue={false}
-              component={({ input }) => (
-                <FormControlLabel
-                  {...input}
-                  control={
-                    <Checkbox
-                      checked={input.value}
-                      color="default"
-                      value="checkedG"
-                    />
-                  }
-                  label="check the box if under warranty"
-                />
-              )}
-            />
+          <div className="row">
+
+
+
+
+            <div className="checkboxContainer">
+              <Field
+                name="warranty"
+                defaultValue={false}
+                component={({ input }) => (
+                  <FormControlLabel
+                    {...input}
+                    control={
+                      <Checkbox
+                        checked={input.value}
+                        color="default"
+                        value="checkedG"
+                      />
+                    }
+                    label="check the box if under warranty"
+                  />
+                )}
+              />
+
+              <Field
+                name="replacementCaseIssued"
+                defaultValue={false}
+                component={({ input }) => (
+                  <FormControlLabel
+                    {...input}
+                    control={
+                      <Checkbox
+                        checked={input.value}
+                        color="default"
+                        value="checkedG"
+                      />
+                    }
+                    label="replacement case issued"
+                  />
+                )}
+              />
+
+              <Field
+                name="damagedBy3rdParty"
+                defaultValue={false}
+                component={({ input }) => (
+                  <FormControlLabel
+                    {...input}
+                    control={
+                      <Checkbox
+                        checked={input.value}
+                        color="default"
+                        value="checkedG"
+                      />
+                    }
+                    label="damaged by 3rd party (airline)"
+                  />
+                )}
+              />
+
+
+
+
+            </div>
           </div>
         </div>
       </form>
